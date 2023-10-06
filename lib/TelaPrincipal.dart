4cbebd883458504bf5620561aba2a1363672a71c
Clayton Ramalho Silva <cdston@gmail.com>
2023-10-06 11:36:10 -0300

    first comment

---
+++ lib/TelaPrincipal.dart
@@ -0,0 +1,87 @@
+import 'package:atm/TelaCliente.dart';
+import 'package:atm/TelaContato.dart';
+import 'package:atm/TelaEmpresa.dart';
+import 'package:atm/TelaServico.dart';
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  void _abrirEmpresa() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => TelaEmpresa()));
+  }
+
+  void _abrirServico() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => TelaServico()));
+  }
+
+  void _abrirCliente() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => TelaCliente()));
+  }
+
+  void _abrirContato() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => TelaContato()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("ATM Consultoria"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/logo.png'),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirServico,
+                    child: Image.asset("images/menu_servico.png"),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: _abrirCliente,
+                    child: Image.asset("images/menu_cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirContato,
+                    child: Image.asset("images/menu_contato.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:atm/TelaPrincipal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
